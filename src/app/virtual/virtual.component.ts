52534d74a3e44f67ab9e467049b751ff73ad3641
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-23 13:50:07 -0600

    base

--- src/app/virtual/virtual.component.ts
+++ src/app/virtual/virtual.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualComponent implements OnInit {
 
+  personas = Array(500).fill(0);
+
+
   constructor() { }
 
   ngOnInit() {
